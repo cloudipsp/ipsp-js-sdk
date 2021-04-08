245adce406868df64d4de1a09b700af698442b70
Stepan Kosatyi <stepan@kosatyi.com>
2021-04-08 22:03:32 +0300

    update

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,11 @@
-/**
- * Declaration for ipsp-js-sdk
- * checkout.min.js version: v2.0.0
- */
+type ClassInstance = {}
 
-declare module 'ipsp-js-sdk' {
-    type ClassInstance = {
-
-    }
-    type ClassObject = {
-        new(params?: Object, ...otherProps): ClassInstance
-        extend(instance?: Object): ClassObject
-    }
-    export default class Component {
-        static get(name:string,params:Object): ClassObject | any;
-        static add(name:string, module: ClassObject ): ClassObject | any;
-    }
+type ClassObject = {
+    new(params?: Object, ...otherProps): ClassInstance
+    extend(instance?: Object): ClassObject
 }
 
+export default class Component {
+    static get(name: string, params: Object): ClassObject | any;
+    static add(name: string, module: ClassObject): ClassObject | any;
+}
