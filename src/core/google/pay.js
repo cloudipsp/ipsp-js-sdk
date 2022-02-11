20c257e9b2dc9fd927d6a3870cdf056fa287054f
Stepan Kosatyi <stepan@kosatyi.com>
2022-02-11 11:23:25 +0200

    update pay.js

--- src/core/google/pay.js
+++ src/core/google/pay.js
@@ -3,60 +3,60 @@ var Module = require('../module');
 var config = require('../config');
 
 var GooglePay = Module.extend({
-    'id':'google-payments-api',
+    'id': 'google-payments-api',
     'init': function () {
-        this.client  = null;
+        this.client = null;
         this.wrapper = this.utils.querySelector('head');
-        this.defer   = Deferred();
+        this.defer = Deferred();
     },
-    'load': function(){
-        if( this.utils.getPath('google.payments.api.PaymentsClient') ) {
+    'load': function () {
+        if (this.utils.getPath('google.payments.api.PaymentsClient')) {
             return this.defer.resolveWith(this);
         }
-        if( this.utils.querySelector('#'.concat(this.id)) ){
+        if (this.utils.querySelector('#'.concat(this.id))) {
             return this.defer;
         }
         this.script = this.utils.createElement('script');
-        this.addAttr(this.script,{
-            id: this.id ,
-            async:true,
-            src:config.GooglePayApi
+        this.addAttr(this.script, {
+            id: this.id,
+            async: true,
+            src: config.GooglePayApi
         });
         this.utils.isElement(this.wrapper) && this.wrapper.appendChild(this.script);
-        this.addEvent(this.script,'load', 'onLoadSuccess');
-        this.addEvent(this.script,'error', 'onLoadError');
+        this.addEvent(this.script, 'load', 'onLoadSuccess');
+        this.addEvent(this.script, 'error', 'onLoadError');
         return this.defer;
     },
-    'show': function(methods){
-        var method = methods.find(function(item){
+    'show': function (methods) {
+        var method = methods.find(function (item) {
             return item.supportedMethods === 'https://google.com/pay';
         });
-        var client = this.getClient();
+        var client = this.getClient({environment: method.data.environment});
         return client.loadPaymentData(method.data);
     },
-    'readyToPay': function(cx,response){
+    'readyToPay': function (cx, response) {
         if (response.result) {
             this.defer.resolveWith(this);
         }
     },
-    'onError': function(cx,error){
-        this.defer.rejectWith(this,error);
+    'onError': function (cx, error) {
+        this.defer.rejectWith(this, error);
     },
-    'onLoadSuccess': function(){
+    'onLoadSuccess': function () {
         this.getClient().isReadyToPay(config.GoogleBaseRequest)
             .then(this.proxy('readyToPay'))
             .catch(this.proxy('onError'));
     },
-    'onLoadError': function(){
+    'onLoadError': function () {
         this.defer.rejectWith(this);
     },
-    'getClient': function(options){
-        if (this.client === null) {
+    'getClient': function (options) {
+        if( options || this.client === null ) {
             var PaymentClient = this.utils.getPath('google.payments.api.PaymentsClient');
-            if( PaymentClient ) {
+            if (PaymentClient) {
                 this.client = new PaymentClient(options);
             } else {
-                this.onError(null, new Error('Google Client Error') );
+                this.onError(null, new Error('Google Client Error'));
             }
         }
         return this.client;
