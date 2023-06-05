783271dae38bc8dcae6c00d88ac909d37a26b0a3
Stepan Kosatyi <stepan@kosatyi.com>
2023-06-05 23:53:49 +0300

    refactor project

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import terser from '@rollup/plugin-terser'
+import pkg from './package.json'
+
+export default {
+    input: pkg.main,
+    output: [
+        {
+            file: 'dist/checkout.js',
+            format: 'umd',
+            name: '$checkout'
+        },
+        {
+            file: 'dist/checkout.min.js',
+            format: 'umd',
+            sourcemap: true,
+            name: '$checkout',
+            plugins:[
+                terser()
+            ]
+        },
+    ],
+    plugins: [
+        commonjs(),
+        resolve({}),
+        babel({
+            presets: ['@babel/preset-env'],
+            babelHelpers: 'bundled'
+        })
+    ]
+}
