ec95f82911b447679a1c027f420ea0e2f66575b5
Stepan Kosatyi <stepan@kosatyi.com>
2023-06-16 17:18:34 +0300

    refactor project

--- src/core/utils.js
+++ src/core/utils.js
@@ -3,104 +3,17 @@ const keyBreaker = /([^\[\]]+)|(\[\])/g;
 const paramTest = /([^?#]*)(#.*)?$/;
 const r20 = /%20/g;
 const rbracket = /\[\]$/;
-
 const matchSymbol = /\s([a-zA-Z]+)/
-
-const getType = exports.getType = (o) => {
-    return ({}).toString.call(o).match(matchSymbol)[1].toLowerCase();
-}
-
-const isObject = exports.getObject = (o) => {
-    return getType(o) === 'object';
-}
-
-const isPlainObject = exports.isPlainObject = (o) => {
-    return (!!o && typeof o === 'object' && o.constructor === Object);
-}
-
-const isFunction = exports.isFunction = (o) => {
-    return getType(o) === 'function'
-}
-
-const isRegexp = exports.isRegexp = (o) => {
-    return getType(o) === 'regexp';
-}
-
-const isArguments = exports.isArguments = (o) => {
-    return getType(o) === 'arguments';
-}
-
-const isError = exports.isError = (o) => {
-    return getType(o) === 'error';
-}
-
-const isArray = exports.isArray = (o) => {
-    return getType(o) === 'array';
-}
-
-const isDate = exports.isDate = (o) => {
-    return getType(o) === 'date';
-}
-
-const isString = exports.isString = (o) => {
-    return getType(o) === 'string';
-}
-
-const isNumber = exports.isNumber = (o) => {
-    return getType(o) === 'number';
-}
-
-const isElement = exports.isElement = (o) => {
-    return o && o.nodeType === 1;
-}
-
-const toArray = exports.toArray = (o) => {
-    return [].slice.call(o);
-}
-
-const querySelectorAll = exports.querySelectorAll = (o, p) => {
-    return toArray((p || document).querySelectorAll(o))
-}
-
-const querySelector = exports.querySelector = (o, p) => {
-    return (p || document).querySelector(o)
-}
-
-const hasProp = exports.hasProp = (o, v) => {
-    return o && o.hasOwnProperty(v);
-}
-
-const forEach = exports.forEach = (ob, cb, cx) => {
-    for (let p in ob)
-        if (hasProp(ob, p))
-            cb.call(cx || null, ob[p], p);
-}
-
-const map = exports.map = (ob, cb, cx) => {
-    let p, t, r = [];
-    for (p in ob)
-        if (hasProp(ob, p))
-            if ((t = cb.call(cx || null, ob[p], p)) !== undefined)
-                r[p] = t;
-    return r;
-}
-
-const cleanObject = exports.cleanObject = (ob) => {
-    for (let p in ob) {
-        if (hasProp(ob, p)) {
-            if (ob[p].length === 0) {
-                if (isArray(ob)) ob.splice(p, 1);
-                if (isPlainObject(ob)) delete ob[p];
-            } else if (isPlainObject(ob[p])) {
-                cleanObject(ob[p]);
-            }
-        }
-    }
-    return ob;
-}
 
 const prep = (str) => {
     return decodeURIComponent(str.replace(/\+/g, ' '));
+}
+const flatten = (array) => {
+    let result = []
+    forEach(array, (item) => {
+        result = result.concat(isArray(item) ? flatten(item) : item)
+    })
+    return result
 }
 
 const recursiveParams = (prefix, obj, next) => {
@@ -121,119 +34,53 @@ const recursiveParams = (prefix, obj, next) => {
     }
 }
 
-const deparam = exports.deparam = (params) => {
-    const data = {}
-    if (params.charAt(0) === '?') params = params.slice(1)
-    if (params && paramTest.test(params)) {
-        forEach(params.split('&'), (pair) => {
-            let parts = pair.split('='),
-                key = prep(parts.shift()),
-                value = prep(parts.join('=')),
-                current = data;
-            if (key) {
-                parts = key.match(keyBreaker);
-                for (let j = 0, l = parts.length - 1; j < l; j++) {
-                    if (!current[parts[j]]) {
-                        current[parts[j]] = digitTest.test(parts[j + 1]) || parts[j + 1] === '[]' ? [] : {};
-                    }
-                    current = current[parts[j]];
-                }
-                const lastPart = parts.pop();
-                if (lastPart === '[]') {
-                    current.push(value);
-                } else {
-                    current[lastPart] = value;
-                }
-            }
-        });
-    }
-    return data;
+const getType = exports.getType = (o) => {
+    return ({}).toString.call(o).match(matchSymbol)[1].toLowerCase();
+}
+const isPlainObject = exports.isPlainObject = (o) => {
+    return (!!o && typeof o === 'object' && o.constructor === Object);
 }
 
-const flatten = exports.flatten = (array) => {
-    let result = []
-    forEach(array,(item)=>{
-        result = result.concat(isArray(item) ? flatten(item) : item)
-    })
-    return result
+const isFunction = exports.isFunction = (o) => {
+    return getType(o) === 'function'
 }
 
-const param = exports.param = (obj) => {
-    const result = [];
-    forEach(obj, (item, prop) => {
-        recursiveParams(prop, item, (key, value) => {
-            value = value == null ? "" : value;
-            result.push([encodeURIComponent(key), encodeURIComponent(value)].join('='));
-        });
-    });
-    return result.join("&").replace(r20, '+');
+const isArray = exports.isArray = (o) => {
+    return getType(o) === 'array';
 }
 
-const removeElement = exports.removeElement = (el) => {
-    el.parentNode.removeChild(el);
+const isElement = exports.isElement = (o) => {
+    return o && o.nodeType === 1;
 }
 
-const createElement = exports.createElement = (tag) => {
-    return document.createElement(tag);
+const toArray = exports.toArray = (o) => {
+    return [].slice.call(o);
 }
 
-
-
-const addClass = exports.addClass = (...args) => {
-    const el = args.shift()
-    if(isElement(el) === false) return;
-    const classList = el.className.trim().split(/\s+/)
-    const tokens = flatten(args.map(item=>item.trim().split(/\s+/)));
-    tokens.forEach(token => {
-        if (token && !~classList.indexOf(token)) {
-            classList.push(token)
-        }
-    })
-    el.className = classList.join(' ').trim()
+const hasProp = exports.hasProp = (o, v) => {
+    return o && o.hasOwnProperty(v);
 }
 
-const removeClass = exports.removeClass = (...args) => {
-    const el = args.shift()
-    if(isElement(el) === false) return;
-    const classList = el.className.trim().split(/\s+/)
-    const tokens = flatten(args.map(item=>item.trim().split(/\s+/)));
-    tokens.forEach((token) => {
-        if( token) {
-            const index = classList.indexOf(token)
-            if(!!~index){
-                classList.splice(index, 1)
+const forEach = exports.forEach = (ob, cb, cx) => {
+    for (let p in ob)
+        if (hasProp(ob, p))
+            cb.call(cx || null, ob[p], p);
+}
+
+const cleanObject = exports.cleanObject = (ob) => {
+    for (let p in ob) {
+        if (hasProp(ob, p)) {
+            if (ob[p].length === 0) {
+                if (isArray(ob)) ob.splice(p, 1);
+                if (isPlainObject(ob)) delete ob[p];
+            } else if (isPlainObject(ob[p])) {
+                cleanObject(ob[p]);
             }
         }
-    })
-    el.className = classList.join(' ').trim()
-}
-
-const addAttr = exports.addAttr = (el,attrs) => {
-    if(isElement(el)===false) return false;
-    if(isPlainObject(attrs)) {
-        forEach(attrs,(value,name)=>{
-            el.setAttribute(name,value)
-        })
     }
+    return ob;
 }
 
-const removeAttr = exports.removeAttr = (el,attrs) => {
-    if(isElement(el)===false) return false;
-    if(isPlainObject(attrs)) {
-        forEach(attrs,(value,name)=>{
-            el.removeAttribute(name,value)
-        })
-    }
-}
-
-
-
-const getStyle = exports.getStyle = (el, prop, getComputedStyle) => {
-    getComputedStyle = window.getComputedStyle;
-    return (getComputedStyle ? getComputedStyle(el) : el['currentStyle'])[prop.replace(/-(\w)/gi, function (word, letter) {
-        return letter.toUpperCase()
-    })];
-}
 
 const extend = exports.extend = (obj, ...args) => {
     forEach(args, function (o) {
@@ -262,7 +109,157 @@ const uuid = exports.uuid = () => {
     return b;
 }
 
-const getPath = exports.getPath = (path) => {
+exports.isObject = (o) => {
+    return getType(o) === 'object';
+}
+
+exports.isRegexp = (o) => {
+    return getType(o) === 'regexp';
+}
+
+exports.isArguments = (o) => {
+    return getType(o) === 'arguments';
+}
+
+exports.isError = (o) => {
+    return getType(o) === 'error';
+}
+
+exports.isDate = (o) => {
+    return getType(o) === 'date';
+}
+
+exports.isString = (o) => {
+    return getType(o) === 'string';
+}
+
+exports.isNumber = (o) => {
+    return getType(o) === 'number';
+}
+
+exports.map = (ob, cb, cx) => {
+    let p, t, r = [];
+    for (p in ob)
+        if (hasProp(ob, p))
+            if ((t = cb.call(cx || null, ob[p], p)) !== undefined)
+                r[p] = t;
+    return r;
+}
+
+exports.querySelectorAll = (o, p) => {
+    return toArray((p || document).querySelectorAll(o))
+}
+
+exports.querySelector = (o, p) => {
+    return (p || document).querySelector(o)
+}
+
+
+exports.deparam = (params) => {
+    const data = {}
+    if (params.charAt(0) === '?') params = params.slice(1)
+    if (params && paramTest.test(params)) {
+        forEach(params.split('&'), (pair) => {
+            let parts = pair.split('='),
+                key = prep(parts.shift()),
+                value = prep(parts.join('=')),
+                current = data;
+            if (key) {
+                parts = key.match(keyBreaker);
+                for (let j = 0, l = parts.length - 1; j < l; j++) {
+                    if (!current[parts[j]]) {
+                        current[parts[j]] = digitTest.test(parts[j + 1]) || parts[j + 1] === '[]' ? [] : {};
+                    }
+                    current = current[parts[j]];
+                }
+                const lastPart = parts.pop();
+                if (lastPart === '[]') {
+                    current.push(value);
+                } else {
+                    current[lastPart] = value;
+                }
+            }
+        });
+    }
+    return data;
+}
+
+exports.param = (obj) => {
+    const result = [];
+    forEach(obj, (item, prop) => {
+        recursiveParams(prop, item, (key, value) => {
+            value = value == null ? "" : value;
+            result.push([encodeURIComponent(key), encodeURIComponent(value)].join('='));
+        });
+    });
+    return result.join("&").replace(r20, '+');
+}
+
+exports.removeElement = (el) => {
+    el.parentNode.removeChild(el);
+}
+
+exports.createElement = (tag) => {
+    return document.createElement(tag);
+}
+
+exports.addClass = (...args) => {
+    const el = args.shift()
+    if (isElement(el) === false) return;
+    const classList = el.className.trim().split(/\s+/)
+    const tokens = flatten(args.map(item => item.trim().split(/\s+/)));
+    tokens.forEach(token => {
+        if (token && !~classList.indexOf(token)) {
+            classList.push(token)
+        }
+    })
+    el.className = classList.join(' ').trim()
+}
+
+exports.removeClass = (...args) => {
+    const el = args.shift()
+    if (isElement(el) === false) return;
+    const classList = el.className.trim().split(/\s+/)
+    const tokens = flatten(args.map(item => item.trim().split(/\s+/)));
+    tokens.forEach((token) => {
+        if (token) {
+            const index = classList.indexOf(token)
+            if (!!~index) {
+                classList.splice(index, 1)
+            }
+        }
+    })
+    el.className = classList.join(' ').trim()
+}
+
+exports.removeAttr = (el, attrs) => {
+    if (isElement(el) === false) return false;
+    if (isPlainObject(attrs)) {
+        forEach(attrs, (value, name) => {
+            el.removeAttribute(name, value)
+        })
+    }
+}
+
+exports.addAttr = (el, attrs) => {
+    if (isElement(el) === false) return false;
+    if (isPlainObject(attrs)) {
+        forEach(attrs, (value, name) => {
+            el.setAttribute(name, value)
+        })
+    }
+}
+
+
+exports.getStyle = (el, prop, getComputedStyle) => {
+    getComputedStyle = window.getComputedStyle;
+    return (getComputedStyle ? getComputedStyle(el) : el['currentStyle'])[prop.replace(/-(\w)/gi, function (word, letter) {
+        return letter.toUpperCase()
+    })];
+}
+
+
+exports.getPath = (path) => {
     const props = path.split('.');
     const first = props.shift();
     let value = null;
@@ -275,19 +272,19 @@ const getPath = exports.getPath = (path) => {
     return value;
 }
 
-const stringFormat = exports.stringFormat = (format, params, expr) => {
+exports.stringFormat = (format, params, expr) => {
     return (format || '').replace(expr || /{(.+?)}/g, (match, prop) => {
         return params[prop] || match;
     });
 }
 
-const cssUnit = exports.cssUnit = (value, unit) => {
+exports.cssUnit = (value, unit) => {
     return String(value || 0).concat(unit || '').concat(' !important')
 }
 
-const getPaymentRequest = exports.getPaymentRequest = ((cx) => {
+exports.getPaymentRequest = ((cx) => {
     let NativePaymentRequest;
-    if( hasProp(cx,'PaymentRequest') && isFunction(cx.PaymentRequest) ) {
+    if (hasProp(cx, 'PaymentRequest') && isFunction(cx.PaymentRequest)) {
         NativePaymentRequest = cx.PaymentRequest
     }
     return (methods, details, options) => {
@@ -295,9 +292,9 @@ const getPaymentRequest = exports.getPaymentRequest = ((cx) => {
         details = details || {};
         details.id = uuid();
         options = options || {};
-        if(NativePaymentRequest){
+        if (NativePaymentRequest) {
             try {
-                request = new NativePaymentRequest(methods, details,options);
+                request = new NativePaymentRequest(methods, details, options);
             } catch (e) {
                 request = null;
             }
@@ -306,8 +303,7 @@ const getPaymentRequest = exports.getPaymentRequest = ((cx) => {
     }
 })(window)
 
-
-const jsonParse = exports.jsonParse = (value, defaults) => {
+exports.jsonParse = (value, defaults) => {
     try {
         return JSON.parse(value)
     } catch (e) {
